1f34b9b311392c6f151330f0c74b7463f23f622e
Guillermo Quezada <meemocol@gmail.com>
2024-06-28 17:26:48 -0600

    feat: add LookupsV2 schema, decorator and tests

---
+++ test/mock/test_mock_lookups.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Mock::TestTwilio < Minitest::Test
+  def test_mock_client_lookups_v2
+    mock_server_response = { "calling_country_code"=>"1",
+                             "country_code"=>"US",
+                             "phone_number"=>"string",
+                             "national_format"=>"string",
+                             "valid"=>true,
+                             "validation_errors"=>[],
+                             "caller_name"=>nil,
+                             "sim_swap"=>nil,
+                             "call_forwarding"=>nil,
+                             "line_status"=>nil,
+                             "line_type_intelligence"=>nil,
+                             "identity_match"=>nil,
+                             "reassigned_number"=>nil,
+                             "sms_pumping_risk"=>nil,
+                             "phone_number_quality_score"=>nil,
+                             "pre_fill"=>nil,
+                             "url"=>"http://example.com"}
+
+    stub_request(:get, "http://twilio_mock_server:4010/v2/PhoneNumbers/+1123456789?Fields=line_type_intelligence").
+      with(
+        headers: {
+          'Accept'=>'application/json',
+          'Accept-Charset'=>'utf-8',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Authorization'=>'Basic QUNGRkZGRkZGRkZGRkZGRkZGRkZGRkZGRkZGRkZGRkZGRjpTS1hYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhY',
+          'User-Agent'=>'twilio-ruby/6.7.1 (linux x86_64) Ruby/3.2.2'
+        }).
+        to_return(status: 200, body: mock_server_response.to_json, headers: {})
+
+    mock_client = Mock::Twilio::Client.new
+    client = ::Twilio::REST::Client.new(nil, nil, nil, nil, mock_client)
+    response = client.lookups.v2.phone_numbers("+1123456789") .fetch(fields: :line_type_intelligence)
+
+    assert_equal "mock", response.line_type_intelligence["type"]
+    assert response.valid
+  end
+end
