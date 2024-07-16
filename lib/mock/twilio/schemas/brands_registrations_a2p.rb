9f8d262b1007ce1df6f04d69f684047ba6af356f
Guillermo Quezada <guillermo.quezada@schoolstatus.com>
2024-07-16 10:38:56 -0600

    feat: Add Support BrandsRegistratiosA2p  and tests (#13)

---
+++ lib/mock/twilio/schemas/brands_registrations_a2p.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Mock
+  module Twilio
+    module Schemas
+      class BrandsRegistrationsA2p
+        class << self
+          def for(body, request)
+            body["date_updated"] = Time.current.rfc2822 if body["date_updated"]
+            body["date_created"] = Time.current.rfc2822 if body["date_created"]
+
+            brand_sid(body) if body["sid"]
+            body["account_sid"] = ::Twilio.account_sid  if body["account_sid"]
+            body["customer_profile_bundle_sid"] = request.data["CustomerProfileBundleSid"] if body["customer_profile_bundle_sid"]
+            body["a2p_profile_bundle_sid"] = request.data["A2PProfileBundleSid"] if body["a2p_profile_bundle_sid"]
+            body["brand_type"] = request.data["BrandType"] if body["brand_type"]
+            body["account_sid"] = ::Twilio.account_sid  if body["account_sid"]
+
+            body["status"] = "PENDING" if body["status"]
+            body["brand_score"] = 100 if body["brand_score"]
+            body["identity_status"] = "SELF_DECLARED" if body["identity_status"]
+            body["tcr_id"] = "BXXXXXX" if body["tcr_id"]
+            body["tax_exempt_status"] = "501c3" if body["tax_exempt_status"]
+
+            body["errors"] = nil if body["errors"]
+            body["failure_reason"] = nil if body["failure_reason"]
+            body["links"] = { "brand_vettings": "http://example.com", "brand_registration_otps": "http://example.com" } if body["links"]
+
+            body
+          end
+
+          def brand_sid(body)
+            prefix = "BN"
+            sid = prefix + SecureRandom.hex(16)
+            scheduler = Rufus::Scheduler.new
+            scheduler.in '2s' do
+              response = Mock::Twilio::Webhooks::Brands.trigger(sid, "unverified")
+              response = if response.status == 200
+                           Mock::Twilio::Webhooks::Brands.trigger(sid, "verified")
+                         end
+            end
+            body["sid"] = sid
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/mock/test_mock_brands.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Mock::TestTwilio < Minitest::Test
+
+  def test_mock_brands
+    mock_server_response = { "sid"=>"stringstringstringstringstringstri",
+                             "account_sid"=>"stringstringstringstringstringstri",
+                             "customer_profile_bundle_sid"=>"stringstringstringstringstringstri",
+                             "a2p_profile_bundle_sid"=>"stringstringstringstringstringstri",
+                             "date_created"=>"2019-08-24T14:15:22Z",
+                             "date_updated"=>"2019-08-24T14:15:22Z",
+                             "brand_type"=>"string",
+                             "status"=>"PENDING",
+                             "tcr_id"=>"string",
+                             "failure_reason"=>"string",
+                             "errors"=>[nil],
+                             "url"=>"http://example.com",
+                             "brand_score"=>0,
+                             "brand_feedback"=>["TAX_ID"],
+                             "identity_status"=>"SELF_DECLARED",
+                             "russell_3000"=>true,
+                             "government_entity"=>true,
+                             "tax_exempt_status"=>"string",
+                             "skip_automatic_sec_vet"=>true,
+                             "mock"=>true,
+                             "links"=>{}}
+
+    stub_request(:post, "http://twilio_mock_server:4010/v1/a2p/BrandRegistrations").
+      with(
+        body: {"A2PProfileBundleSid"=>"BUFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", "BrandType"=>"STANDARD", "CustomerProfileBundleSid"=>"BUFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", "Mock"=>"true"},
+        headers: {
+          'Accept'=>'application/json',
+          'Accept-Charset'=>'utf-8',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Authorization'=>'Basic QUNGRkZGRkZGRkZGRkZGRkZGRkZGRkZGRkZGRkZGRkZGRjpTS1hYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhY',
+          'Content-Type'=>'application/x-www-form-urlencoded',
+          'User-Agent'=>'twilio-ruby/7.1.0 (linux x86_64) Ruby/3.2.2'
+        }).
+        to_return(status: 200, body: mock_server_response.to_json, headers: {})
+
+
+    twilio_params = { customer_profile_bundle_sid: "BUFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                      a2p_profile_bundle_sid: "BUFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                      brand_type: 'STANDARD',
+                      mock: true }
+
+    mock_client = Mock::Twilio::Client.new
+    client = ::Twilio::REST::Client.new(nil, nil, nil, nil, mock_client)
+    response = client.messaging.v1.brand_registrations.create(**twilio_params)
+
+    assert Time, response.date_created.class
+    assert Time, response.date_updated.class
+
+    assert_equal "BN", response.sid[0,2]
+    assert_equal "PENDING", response.status
+    assert_equal "STANDARD", response.brand_type
+    assert_equal "SELF_DECLARED", response.identity_status
+  end
+end
