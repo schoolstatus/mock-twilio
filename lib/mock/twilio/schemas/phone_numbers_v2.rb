2daf00f4857c365bc5ddd4b2327bc6aaa871f1cf
Guillermo Quezada <guillermo.quezada@schoolstatus.com>
2024-11-12 16:41:41 -0600

    chore: add line_type_intelligence mobile and landline (#23)

--- lib/mock/twilio/schemas/phone_numbers_v2.rb
+++ lib/mock/twilio/schemas/phone_numbers_v2.rb
@@ -10,9 +10,18 @@ module Mock
             body["country_code"] = 'US' if body["country_code"]
             body["valid"] = true if body["valid"]
             body["validation_errors"] = [] if body["validation_errors"]
-            body["line_type_intelligence"] = { "carrier_name" => "Mock::Twilio - SMS/MMS-SVR", "type" => "mock" }
+            body["line_type_intelligence"] = { "carrier_name" => "Mock::Twilio - SMS/MMS-SVR", "type" => line_type(request) }
 
             body
+          end
+
+          private
+
+          def line_type(request)
+            # https://www.twilio.com/docs/lookup/v2-api/line-type-intelligence#type-property-values
+            return "landline" if request.url[-1] == "9"
+
+            "mobile"
           end
         end
       end
